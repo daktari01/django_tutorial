631d33f005bb678320919359845f1afc76728140
James Dindi <dindijjames@gmail.com>
2017-11-24 22:49:37 +0300

    [session 26]: Use Django process_view Middleware Hook

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+
 def home(request):
     numbers = [1, 2, 3, 4, 5]
     name = 'Jane Doe'
