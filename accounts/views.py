d7bb0d2ea8bbe3996118d5783f43cfe4ebdbb5cd
James Dindi <dindijjames@gmail.com>
2017-11-26 03:41:44 +0300

    [session 29]: Avoid hard coded URLs in django

--- accounts/views.py
+++ accounts/views.py
@@ -30,12 +30,10 @@ def register(request):
         args = {'form': form}
         return render(request, 'accounts/reg_form.html', args)
 
-@login_required
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
-@login_required
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user) 
@@ -49,7 +47,6 @@ def edit_profile(request):
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
 
-@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user) 
--- tutorial/middleware.py
+++ tutorial/middleware.py
@@ -1,6 +1,7 @@
 import re
 
 from django.conf import settings
+from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 
@@ -19,14 +20,8 @@ class LoginRequiredMiddleware:
     def process_view(self, request, view_func, view_args, view_kwargs):
         assert hasattr(request, 'user')
         path = request.path_info.lstrip('/')
-        print(path)
-
-        # if not request.user.is_authenticated():
-        #     if not any(url.match(path) for url in EXEMPT_URLS):
-        #         return redirect(settings.LOGIN_URL)
-
         url_is_exempt = any(url.match(path) for url in EXEMPT_URLS)
-        if path == 'accounts/logout/':
+        if path == reverse('logout'):
             logout(request)
 
         if request.user.is_authenticated() and url_is_exempt:
