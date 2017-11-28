de7a72f07f00e1ea590b4ff9db2a9ee6d1ca510c
James Dindi <dindijjames@gmail.com>
2017-11-28 21:53:43 +0300

    [session 41]: Different ways to have the django server running

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 
 class UserProfileManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(city='Las Vegas, NV')
+        return super(UserProfileManager, self).get_queryset().filter(city='Las Vegas, NV')
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
