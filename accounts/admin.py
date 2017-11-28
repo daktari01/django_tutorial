7b276636e0f8ad84964c50e03424dfc76a8dd46d
James Dindi <dindijjames@gmail.com>
2017-11-28 09:25:03 +0300

    [session 38]: Add columns to Django admin model pages

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from accounts.models import UserProfile
 
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.site_header = 'Marvelus Administration'
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city', 'phone', 'website', 'user_info')
+
+    def user_info(self, obj):
+        return obj.description 
+
+    user_info.short_description = 'Info'
+
+admin.site.register(UserProfile, UserProfileAdmin)
