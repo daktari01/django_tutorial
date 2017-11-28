b0c84a4eb003bdc8ef78e51011a38029e28a3460
James Dindi <dindijjames@gmail.com>
2017-11-28 23:21:50 +0300

    [session 43]: Create simple home page using class based views

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from . import views
+from home.views import HomeView
 
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
