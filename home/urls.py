6b0e55c8db8046a9ea1acb138b87b027023935d2
James Dindi <dindijjames@gmail.com>
2017-11-30 05:28:34 +0300

    [session 46]: Receive data from a django form using a POST request

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from home.views import HomeView
 
 
 urlpatterns = [
+    #url(r'post/(?P<block_id>\d+)/$', HomeView.as_view(), name='home'),
     url(r'^$', HomeView.as_view(), name='home'),
+    #post/(?P<block_id>\d+)/$
 ]
